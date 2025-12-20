ee2de108634cbee34a02346f38d844eafb07adfc
UmutSen2662 <163651345+UmutSen2662@users.noreply.github.com>
2025-12-20 17:37:07 +0000

    Deploying to gh-pages from @ cnzkn/RFIDAttendanceSystem@56f882ffaff7ba3a26056cc5be74e1c6daa813aa 🚀

--- assets/courses-D0yvIoc9.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-CcUVUUKD.js";function n(){return o.jsx("div",{children:'Hello "/courses"!'})}export{n as component};
---
+++ assets/useSuspenseQuery-B7eiKyWE.js
@@ -0,0 +1,1 @@
+import{E as r,Q as u,F as s}from"./index-DL-5RPEL.js";function t(e,a){return r({...e,enabled:!0,suspense:!0,throwOnError:s,placeholderData:void 0},u)}export{t as u};
